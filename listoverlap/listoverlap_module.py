2b03fc3d03f200617998d4c35aef185e460b52dd
KrisztaAnna <krisztina.baranyai@gmail.com>
2016-05-23 13:36:21 +0200

    Add listoverlap

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,9 +1,15 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+
 def listoverlap(list1, list2):
-    return
+    list3 = set(list1) & set(list2)
+    return list3
 
 
 def main():
-    return
+    xxx = listoverlap(a, b)
+    return xxx
 
 
 if __name__ == '__main__':
