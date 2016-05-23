001f882e402169fd35eaf0abbd9f1866b23e2a2d
KrisztaAnna <krisztina.baranyai@gmail.com>
2016-05-23 11:53:59 +0200

    Add fizzbuzz

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,19 @@
 def fizzbuzz(number):
-    return
+    to_return = ""
+    if number % 3 == 0 and number % 5 == 0:
+        to_return = "FizzBuzz"
+    elif number % 5 == 0:
+        to_return = "Buzz"
+    elif number % 3 == 0:
+        to_return = "Fizz"
+    else:
+        to_return = number
+    return to_return
 
 
 def main():
+    for i in range(1,101):
+        print(fizzbuzz(i))
     return
 
 if __name__ == '__main__':
