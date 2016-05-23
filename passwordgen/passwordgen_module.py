cac1998e28a69ec9f20f7a85926f53f343ca0167
balag3 <carbalage@gmail.com>
2016-05-23 14:12:30 +0200

    First change

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,9 +1,34 @@
+import string
+import random
+
+# char_list = string.printable
+# print(char_list)
+# password = ""
+# length = random.randint(8,16)
+# print(length)
+# while len(password) < length:
+#     my_random = char_list[random.randint(0,len(char_list)-1)]
+#     password = password + my_random
+#
+# print(password)
+
+
+
+
+
 def passwordgen():
-    return
+    char_list = string.printable
+    password = ""
+    length = random.randint(8,16)
+    while len(password) < length:
+        my_random = char_list[random.randint(0,len(char_list)-1)]
+        password = password + my_random
+    return password
 
 
 def main():
-    return
+    solution = passwordgen()
+    return solution
 
 
 if __name__ == '__main__':
