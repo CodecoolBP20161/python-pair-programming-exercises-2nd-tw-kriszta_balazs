17039fab828a12990c261527b0ef5e5094fd5260
balag3 <carbalage@gmail.com>
2016-05-23 13:16:04 +0200

    First rotation round starts

--- years/years_module.py
+++ years/years_module.py
@@ -1,12 +1,20 @@
-import datetime
+from datetime import *
+
+
 
 
 def years(age):
-    return
+    this_year = date.today().year
+    year = (100 - age)+this_year
+    return year
 
 
 def main():
-    return
+    name = input("What is your name?")
+    age = int(input("What is your age?"))
+    solution = years(age)
+    print("You are going to be")
+    return solution
 
 
 if __name__ == '__main__':
