e6c2732b618589cfc422bf5e090759fb1e413395
KrisztaAnna <krisztina.baranyai@gmail.com>
2016-05-24 09:56:57 +0200

    Add

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -5,20 +5,20 @@ import random
 
 lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-nums = str(range(0,10))
+#nums = str(range(0,10))
 chars = "[!@#$%^&*()?]"
 
 def passwordgen():
     password = ""
-    length = random.randint(8,16)
+    length = random.randint(8,17)
     while len(password) < length:
-        rand_low = lower[random.randint(0,len(lower))]
+        rand_low = lower[random.randint(0,len(lower)-1)]
         password = password + rand_low
-        rand_up = upper[random.randint(0, len(upper))]
+        rand_up = upper[random.randint(0, len(upper)-1)]
         password = password + rand_up
-        rand_num = nums[random.randint(0, len(nums))]
-        password = password + rand_num
-        rand_chars = chars[random.randint(0, len(chars))]
+        #rand_num = nums[random.randint(0, len(nums)-1)]
+        password = password + str(range(10))
+        rand_chars = chars[random.randint(0, len(chars)-1)]
         password = password + rand_chars
 
 
